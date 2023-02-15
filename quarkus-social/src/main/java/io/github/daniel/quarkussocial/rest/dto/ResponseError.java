48388dcdc183476f5d41effbe07c16564f9594f9
daniel <gitlab@shift.com.br>
2023-02-14 23:18:35 -0300

    aula 25 alterações

--- quarkus-social/src/main/java/io/github/daniel/quarkussocial/rest/dto/ResponseError.java
+++ quarkus-social/src/main/java/io/github/daniel/quarkussocial/rest/dto/ResponseError.java
@@ -1,12 +1,15 @@
 package io.github.daniel.quarkussocial.rest.dto;
 
 import javax.validation.ConstraintViolation;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ResponseError {
+
+    public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
     //contem a menssagem
     private String message;
@@ -37,4 +40,8 @@ public class ResponseError {
     public Collection<FieldError> getErrors() {return errors;}
 
     public void setErrors(Collection<FieldError> errors) {this.errors = errors;}
+
+    public Response withStatusCode(int code){
+        return Response.status(code).entity(this).build();
+    }
 }
